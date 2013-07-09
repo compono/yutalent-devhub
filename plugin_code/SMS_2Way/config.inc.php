d3d02c254752d4f145870bded6eeaf956b1d6047
jigagile <jpatel@agileinfoways.com>
2013-07-09 16:11:05 +0530

    config include test

--- plugin_code/SMS_2Way/config.inc.php
+++ plugin_code/SMS_2Way/config.inc.php
@@ -2,4 +2,5 @@
 //defining file path for both devloper and product version.
 define("DEV",     "d/plugin_dev/plugin_code/SMS_2Way/");
 define("PROD",     "d/plugin_prod/plugin_code/SMS_2Way/");
+echo "this file is included";
 ?>
