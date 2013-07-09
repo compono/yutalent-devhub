33271dfdbe8b64532d8cfd4f33e820cc6e911b28
jigagile <jpatel@agileinfoways.com>
2013-07-09 17:11:49 +0530

    adding final path to files

--- plugin_code/SMS_2Way/config.inc.php
+++ plugin_code/SMS_2Way/config.inc.php
@@ -1,6 +1,6 @@
 <?php 
 //defining file path for both devloper and product version.
-define("SITE_URL",     "https://www.yutalent.com/");
+define("SITE_URL",     "/home/yutalent.com/public_html/");
 define("DEV",     "d/plugin_dev/plugin_code/SMS_2Way/");
 define("PROD",     "d/plugin_prod/plugin_code/SMS_2Way/");
 ?>
--- plugin_code/SMS_2Way/views/sms_popup.php
+++ plugin_code/SMS_2Way/views/sms_popup.php
@@ -52,13 +52,9 @@ WU.init({
 <body>
 <?php 
 // including required files
-require_once('../config.inc.php');
+require_once('/home/yutalent.com/public_html/d/plugin_dev/plugin_code/SMS_2Way/config.inc.php');
 require_once('/home/developers_sandbox/SMS_2Way_config.php');
-
-//echo SITE_URL.DEV.'libraries/textmagicAPI/TextMagicAPI.php';
-
 require_once('/home/yutalent.com/public_html/d/plugin_dev/plugin_code/SMS_2Way/libraries/textmagicAPI/TextMagicAPI.php');
-//echo "Hello";
 ?>
 
 <!-- text message validation and sending-->
