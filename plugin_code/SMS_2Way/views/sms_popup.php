ebb7f81528723fb8c080b4a9e5cd28bf77b9a104
jigagile <jpatel@agileinfoways.com>
2013-07-09 17:35:34 +0530

    test

--- plugin_code/SMS_2Way/views/sms_popup.php
+++ plugin_code/SMS_2Way/views/sms_popup.php
@@ -52,8 +52,8 @@ WU.init({
 <body>
 <?php 
 // including required files
-echo SITE_URL.DEV.'config.inc.php';
 require_once('/home/yutalent.com/public_html/d/plugin_dev/plugin_code/SMS_2Way/config.inc.php');
+echo SITE_URL.DEV.'config.inc.php';
 require_once('/home/developers_sandbox/SMS_2Way_config.php');
 require_once('/home/yutalent.com/public_html/d/plugin_dev/plugin_code/SMS_2Way/libraries/textmagicAPI/TextMagicAPI.php');
 ?>
