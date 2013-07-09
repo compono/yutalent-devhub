a9d038d7c4910b1cc1bdc0796375c38e99f84da2
jigagile <jpatel@agileinfoways.com>
2013-07-09 16:08:47 +0530

    include file path

--- plugin_code/SMS_2Way/views/sms_popup.php
+++ plugin_code/SMS_2Way/views/sms_popup.php
@@ -52,7 +52,8 @@ WU.init({
 <body>
 <?php 
 // including required files
-require_once('https://www.yutalent.com/d/plugin_dev/plugin_code/SMS_2Way/config.inc.php');
+require_once('../config.inc.php');
+echo "Hello";
 require_once(DEV.'SMS_2Way_config.php');
 require_once(DEV.'libraries/textmagicAPI/TextMagicAPI.php');
 ?>
