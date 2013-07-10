dbda6ab88f15f5d5b62f2a7ba0720dedf5cf9fc4
Saurav <saurav@anylinuxwork.com>
2013-07-10 14:37:45 +0530

    Fixing storage mailchimp issue

--- plugin_code/MailChimp/mailchimp.js
+++ plugin_code/MailChimp/mailchimp.js
@@ -74,7 +74,7 @@ window.wuAfterInit = function(wu)
 				var dropDown = $('p#'+this.key).find('select');
 				//dropDown.find('option').filter('[webid="'+this.value[0]+'"]').attr('selected','selected');
 				console.log(this.key+'---'+this.value[0]);
-				dropDown.multiselect({ header:false,multiple: false,selectedList: 1});
+				//dropDown.multiselect({ header:false,multiple: false});
 				dropDown.find('option').filter('[webid="'+this.value[0]+'"]').attr('selected','selected');
 				if(!dropDown.val())	dropDown.siblings('button').find('span').addClass('value');
 			});
