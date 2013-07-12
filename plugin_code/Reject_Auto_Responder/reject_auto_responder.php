55582251dd345f16e905a26d43adec8a8dfb2099
Saurav <saurav@anylinuxwork.com>
2013-07-12 18:26:28 +0530

    Add html for the page

--- plugin_code/Reject_Auto_Responder/reject_auto_responder.php
+++ plugin_code/Reject_Auto_Responder/reject_auto_responder.php
@@ -1,18 +1,23 @@
 <link href="https://www.wutalent.co.uk/static/styles/plugin/plugin.css" rel="stylesheet" />
 <style>
+
 #content{padding: 0 25px 25px; width: 95%;}
 .red-title{float: left;padding-bottom: 50px;width: 100%;}
 .radioDiv{text-align:center}
-.font-20{font-weight:bold;}
+.text{padding-top:30px;}
 ul,li{list-style:none}
 .inner {font-size: 18px;padding-bottom: 8px;}
-.inner li{display:inline-block;width:30%;margin-right:3%}
+.inner li{display:inline-block;width:32%;margin-right:1%}
 .inner li.last{margin-right:0}
 .inner li input[type="text"]{  font-size: 18px;padding: 6px;width: 100%;}
-::-webkit-input-placeholder { color:#AA9984 !important; }
+/*::-webkit-input-placeholder { color:#AA9984 !important; }
 ::-moz-placeholder { color:#AA9984 !important; } /* firefox 19+ */
-:-ms-input-placeholder { color:#AA9984 !important; } /* ie */
+/*:-ms-input-placeholder { color:#AA9984 !important; } /* ie */
 input:-moz-placeholder { color:#AA9984 !important; }
+.save-auto{background: url("images/saveAutoResponder.png") no-repeat scroll -1px 0 transparent;display: block;height: 43px;margin-top: 14px;text-decoration: none;width: 367px;float:right}
+.save-auto:hover{background: url("images/saveAutoResponder.png") no-repeat scroll -1px -43px transparent;}
+.smtpSecure {padding-bottom: 30px;padding-top: 20px;}
+.smtpSecure .standard-blue-link{font-size:18px;}
 </style>
 <div id="content">
 	<span class="red-title">Email send method</span>
@@ -51,5 +56,19 @@ input:-moz-placeholder { color:#AA9984 !important; }
                 </ul>
             </li>
         </ul>
+        <div class="smtpSecure">
+        	<div class="goLeft"><a href="" class="standard-blue-link">Click to test connection...</a></div>
+			<div class="goRight">
+            	<span class="bronze">SMTP secure</span>
+                <span class="">
+                	<label><input type="radio"/>None</label>
+                    <label><input type="radio"/>SSL</label>
+                    <label><input type="radio"/>TLS</label>    
+                </span>
+			</div>
+        </div>
+        <div class="">
+        	<a href="" class="save-auto"></a>
+        </div>
     </div>
 </div>
