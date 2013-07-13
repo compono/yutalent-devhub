25efdc0ed8a143b85fdfbf30c18e0bad65f6d0e8
jigagile <jpatel@agileinfoways.com>
2013-07-13 11:07:52 +0530

    alert temp mobile no

--- plugin_code/SMS_2Way/views/sms_popup.php
+++ plugin_code/SMS_2Way/views/sms_popup.php
@@ -16,7 +16,8 @@ console.log( response );
 var mob_no = response.phone.profile.mobile;
 $('#cont_no').val(mob_no);
 
-var temp_mob_no = 919173575883;
+var temp_mob_no = '919173575883';
+alert(temp_mob_no);
 $.ajax({
         url: "msg_form_submit.php?mob="+temp_mob_no,
         type: "post",
