25850ec3cd100e9855f1d56e7d6c0422e643fec2
jigagile <jpatel@agileinfoways.com>
2013-07-13 12:19:05 +0530

    setting form display parameters

--- plugin_code/SMS_2Way/views/sms_popup.php
+++ plugin_code/SMS_2Way/views/sms_popup.php
@@ -20,7 +20,7 @@ $.ajax({
 				$('#sms_form').replaceWith('<div>Invalid mobile no.</div>');
 			}else
 			{
-				
+				$('#sms_form').css('display','block');
 			}
         }
     });
@@ -165,7 +165,7 @@ if(isset($_POST['msg_hide']))
 
 
 <!--html for message form start-->
-<form method="Post" action="" name="sms_popup" id="sms_form"/>
+<form method="Post" action="" name="sms_popup" id="sms_form" style="display:none;"/>
 <input type="hidden" name="signed_request" value="<?php print $_REQUEST['signed_request'] ?>"/>
 <input type="hidden" name="contact_no" id="cont_no" value="">
 <input type="hidden" name="credit_count" id="cred_count" value="">
