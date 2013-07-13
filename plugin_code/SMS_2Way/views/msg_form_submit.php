12b2cd5ac9fa67eafe68b177d1a64d5fcc3ac97b
jigagile <jpatel@agileinfoways.com>
2013-07-13 11:53:47 +0530

    update msg_form_submit.php

--- plugin_code/SMS_2Way/views/msg_form_submit.php
+++ plugin_code/SMS_2Way/views/msg_form_submit.php
@@ -1,6 +1,19 @@
 <?php
+require_once('../config.inc.php');
+require_once('/home/developers_sandbox/SMS_2Way_config.php');
+require_once(SITE_URL.DEV.'libraries/textmagicAPI/TextMagicAPI.php');
 
-echo $mobile_no = $_GET['mob'];
+$mob_no = $_GET['mob'];
+$phones = array($mob_no);
+
+$api = new TextMagicAPI(array(
+			"username" => U_NAME,
+			"password" => U_PASS, 
+		));
+		
+$results = $api->checkNumber($phones);			
+print_r($results);
+
 
 
 ?>
