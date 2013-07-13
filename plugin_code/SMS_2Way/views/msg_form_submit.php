cb716c65145b636e401821f11f13b5af6eec3850
jigagile <jpatel@agileinfoways.com>
2013-07-13 12:06:47 +0530

    remove array

--- plugin_code/SMS_2Way/views/msg_form_submit.php
+++ plugin_code/SMS_2Way/views/msg_form_submit.php
@@ -4,8 +4,7 @@ require_once('/home/developers_sandbox/SMS_2Way_config.php');
 require_once(SITE_URL.DEV.'libraries/textmagicAPI/TextMagicAPI.php');
 
 $mob_no = $_GET['mob'];
-$mob2 = '919978707853';
-$phones = array($mob_no,$mob2);
+$phones = array($mob_no);
 
 $api = new TextMagicAPI(array(
 			"username" => U_NAME,
