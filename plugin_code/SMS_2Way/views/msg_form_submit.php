2b793318dd3b8092be9b0fd085239d79bfeb761d
jigagile <jpatel@agileinfoways.com>
2013-07-13 11:59:29 +0530

    return invalid no

--- plugin_code/SMS_2Way/views/msg_form_submit.php
+++ plugin_code/SMS_2Way/views/msg_form_submit.php
@@ -16,8 +16,8 @@ try {
     $results = $api->checkNumber($phones);
 
     foreach($results as $number => $info) {
-        $price = $info['price'];
-        $country = $info['country'];
+        echo $price = $info['price'];
+        echo $country = $info['country'];
     }
 
 } catch (WrongPhoneFormatException $e) {
