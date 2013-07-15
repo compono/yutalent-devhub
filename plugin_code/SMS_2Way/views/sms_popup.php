eb529a3c9b90fb4a2cf32c1b2c0ca7634ca40464
jigagile <jpatel@agileinfoways.com>
2013-07-15 13:57:13 +0530

    uncomment credit balance

--- plugin_code/SMS_2Way/views/sms_popup.php
+++ plugin_code/SMS_2Way/views/sms_popup.php
@@ -16,7 +16,7 @@ window.wuAfterInit = function(wu) {
 var cid = wu.Options.getOption('request')['id'];
 console.log( cid );
 wu.Messenger.sendMessageToWU('candidates/get', {id: wu.Options.getOption('request')['id'] }, function(response){
-console.log( response ));
+console.log( response );
 /* assigning mobile no to variable from yutalent js API response*/
 var mob_no = response.phone.profile.mobile;
 $('#cont_no').val(mob_no);
@@ -42,9 +42,9 @@ $.ajax({
         }
     });
 	
-//var balance = wu.sendMessagetoWU('credits/getAppCredits', function(response){
-//console.log( response );
-//alert(balance);
+var balance = wu.sendMessagetoWU('credits/getAppCredits', function(response){
+console.log( response );
+alert(balance);
 
 var success = $('#char_count').text();
 if(success == 'success')
