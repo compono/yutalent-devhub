b79f568e5529575020a72a9921fe24ae68a5998a
Saurav <saurav@anylinuxwork.com>
2013-07-17 17:01:43 +0530

    Fixed data encode issue

--- plugin_code/Reject_Auto_Responder/reject.js
+++ plugin_code/Reject_Auto_Responder/reject.js
@@ -1,3 +1,4 @@
+// add custome validator for tinyMce
 jQuery.validator.addMethod("tinyMCEvalidator", function(value, element)
 {
 	$('#full-description').val(tinyMCE.get('full-description').getContent());
@@ -7,13 +8,14 @@ jQuery.validator.addMethod("tinyMCEvalidator", function(value, element)
 }, "Please eneter mail content");
 $(window).load(function()
 {
+	// add tiny mce editor
 	$('textarea.tinymce').tinymce({setup:function(ed){ed.onInit.add(function(ed,evt){tinyMCE.dom.Event.add(ed.getDoc(),'blur',function(e){$('#full-description').blur();});});},script_url:'https://www.wutalent.co.uk//static/scripts/lib/tiny_mce/tiny_mce.js',forced_root_block:'',force_br_newlines:true,force_p_newlines:false,paste_auto_cleanup_on_paste:true,paste_remove_styles:true,paste_remove_styles_if_webkit:true,paste_strip_class_attributes:"all",paste_use_dialog:false,paste_remove_spans:true,paste_remove_styles:true,paste_retain_style_properties:'',paste_text_linebreaktype:'br',convert_newlines_to_brs:true,element_format:"xhtml",fix_list_elements:true,valid_elements:"br,em/i,strong/b,ul,ol,li",paste_preprocess:function(pl,o){o.content=o.content.replace(/<(p|div)\s?[^>]*?>\s*<br\s?\/?>\s*<\/(p|div)>/gi,'<br/>');o.content=o.content.replace(/<(p|div)\s?[^>]*?>/gi,'').replace(/<\/(p|div)>/gi,'<br/>');},theme:"advanced",plugins:"autoresize,autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",theme_advanced_buttons1:"bold,italic,bullist,numlist",theme_advanced_toolbar_location:"top",theme_advanced_toolbar_align:"right",theme_advanced_statusbar_location:"bottom",theme_advanced_resizing:false,height:'200px'});
 });
 $(document).ready(function()
 {
-	$("#rejectAuotRespond").validate({});
-	$('#full-description').rules("add", {tinyMCEvalidator: true});
-	$('#ownSmtp').click(function()
+	$("#rejectAuotRespond").validate({});								// add form validation initilize
+	$('#full-description').rules("add", {tinyMCEvalidator: true});		// add validation for tiny mce editor
+	$('#ownSmtp').click(function()										// remove validations from user's smtp setting
 	{
 		$('.userSmtpSetting').slideUp();
 		$('#fromEmail').rules('remove');  
@@ -22,7 +24,7 @@ $(document).ready(function()
 		$('#userName').rules('remove'); 
 		$('#password').rules('remove'); 
 	});
-	$('#customerSmtp').click(function()
+	$('#customerSmtp').click(function()									// add validations from user's smtp setting
 	{
 		$('.userSmtpSetting').slideDown();
 		$("#fromEmail").rules("add", {required: true,email:true,messages: {required: "Please enter sending email",email:'Please enter valid email'}});
@@ -33,7 +35,7 @@ $(document).ready(function()
 		//$("#port").rules("add", {required: true,messages: {required: "Please enter port"}});										
 	});
 	
-	$('#port').keyup(function()
+	$('#port').keyup(function()											// change smtp secure when user add value in port
 	{
 		var value = $(this).val();
 		if(parseInt(value) == 465)		$('#smtpSSL').trigger('click');
@@ -41,7 +43,7 @@ $(document).ready(function()
 		else							$('#smtpNone').trigger('click');
 	});
 
-	$('#smtpSSL,#smtpTLS,#smtpNone').click(function()
+	$('#smtpSSL,#smtpTLS,#smtpNone').click(function()					// change port value when user choose smtp secure 
 	{
 		var smtpId = $(this).attr('id');
 		if(smtpId == 'smtpSSL')			$('#port').val(465);
@@ -51,7 +53,7 @@ $(document).ready(function()
 
 window.wuAfterInit = function(wu)
 {
-	wu.Messenger.sendMessageToWU('storage/get-multiple',{ keys: ['useSmtp','fromEmail','fromName','hostServer','userName','password','port','mailContent'] },function(response)
+	wu.Messenger.sendMessageToWU('storage/get-multiple',{ keys: ['useSmtp','fromEmail','fromName','hostServer','userName','password','port','mailContent'] },function(response)		// get added information
 	{
 		var formData = new Array();
 		
@@ -61,7 +63,7 @@ window.wuAfterInit = function(wu)
 		});
 		
 		if(Object.keys(formData).length)
-		{
+		{	// add added information in form
 			(parseInt(formData['useSmtp'])) ? $('#ownSmtp').trigger('click') : $('#customerSmtp').trigger('click');
 			$('#fromEmail').val(formData['fromEmail']);
 			$('#fromName').val(formData['fromName']);
@@ -69,14 +71,14 @@ window.wuAfterInit = function(wu)
 			$('#userName').val(formData['userName']);
 			$('#password').val(formData['password']);
 			$('#port').val(formData['port']);
-			$('#full-description').val(decodeURIComponent(formData['mailContent']));
+			$('#full-description').val((formData['mailContent']));
 			$('#rejectAuotRespond').show();			
 			if(parseInt(formData['port']) == 465)		$('#smtpSSL').trigger('click');
 			else if(parseInt(formData['port']) == 587)	$('#smtpTLS').trigger('click');
 			else										$('#smtpNone').trigger('click');
 		}
 		$('#rejectAuotRespond').show();
-		$('a#testSmtpConnection').click(function()
+		$('a#testSmtpConnection').click(function()		// test connection to check smtp setting
 		{
 			var formData = new Array();
 			formData['fromEmail'] 	= 	$('#fromEmail').val();
@@ -85,7 +87,7 @@ window.wuAfterInit = function(wu)
 			formData['userName'] 	= 	$('#userName').val();
 			formData['password'] 	= 	$('#password').val();
 			formData['smtpNumber'] 		= 	$('#port').val();
-			formData['mailContent'] = 	encodeURIComponent($('#full-description').val());
+			formData['mailContent'] = 	($('#full-description').val());
 			formData['port']  = 	$('[name="smtpNumber"]:checked').val();
 			var dataString = Object.keys(formData).map(function(x){return x+'='+formData[x];}).join('&');
 			$.ajax(
@@ -101,7 +103,7 @@ window.wuAfterInit = function(wu)
 			});
 		});
 	});
-	$('#rejectAuotRespond').submit(function()
+	$('#rejectAuotRespond').submit(function()		// submit the form
 	{
 		if($(this).valid())
 		{
@@ -116,7 +118,7 @@ window.wuAfterInit = function(wu)
 			if(parseInt(formData['useSmtp']))
 				formData['mailContent'] = 	$('#full-description').val();
 			else
-				formData['mailContent'] = 	encodeURIComponent($('#full-description').val());
+				formData['mailContent'] = 	($('#full-description').val());
 			wu.Messenger.sendMessageToWU('storage/add-multiple',{append: false, pairs: formData},function(response)
 			{
 				statusMessage('Your settings have been saved',false);
