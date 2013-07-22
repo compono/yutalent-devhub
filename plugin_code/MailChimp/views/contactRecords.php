8c44f964dd4c671a1e9efa908154a8e605d09950
Saurav <saurav@anylinuxwork.com>
2013-07-22 14:02:49 +0530

    Change Mailchimp text

--- plugin_code/MailChimp/views/contactRecords.php
+++ plugin_code/MailChimp/views/contactRecords.php
@@ -1,6 +1,6 @@
 <div class="wuplugin-title"></div>
 <span class="red-title">Map contact records</span>
-<div class="bronze-info">Map the contact record types to the mailing lists you have inside your Mailchimp account. New contacts with emails will be automatically added to the mailing list.</div>
+<div class="bronze-info">Map the contact record types to the mailing lists you have inside your MailChimp account. New contacts with emails will be automatically added to the mailing list.</div>
 <div id="mapContentRecords"></div>
 <?php /* Css and JS files require for select box and send the request to get and add teh data*/ ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
