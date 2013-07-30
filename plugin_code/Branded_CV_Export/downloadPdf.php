77c6f65140081f77c1528b80a9877635748e42a7
Saurav <saurav@anylinuxwork.com>
2013-07-30 16:56:47 +0530

    testing download pdf issue

--- plugin_code/Branded_CV_Export/downloadPdf.php
+++ plugin_code/Branded_CV_Export/downloadPdf.php
@@ -25,11 +25,11 @@ $WU_API = new WU_API();
 // this is optional, but if you use query parameters in your script,
 // then better to set it right, as oauth server will return additional parameters into script
 // and then redirect uri will differ from the url which requested access token
-$WU_API->setRedirectUri($scriptUrl);	/*
+/*$WU_API->setRedirectUri($scriptUrl);	
 $currentUserProfile 	= $WU_API->sendMessageToWU('contacts/get',array('id'=>$id));
-$currentUserProfile	= json_decode(json_encode($currentUserProfile),true);*/
+$currentUserProfile	= json_decode(json_encode($currentUserProfile),true);
 $candidateName 		= $currentUserProfile['name'];
-$userCVDetail 		= $WU_API->sendMessageToWU('contacts/get-parsed-cv',array('id'=>$id));
+$userCVDetail 		= $WU_API->sendMessageToWU('contacts/get-parsed-cv',array('id'=>$id));*/
 $userCVDetail		= json_decode(json_encode($userCVDetail),true);
 $summary 		= str_replace('/strong>',"/strong><br/>",$userCVDetail['html']['summary']);
 //$privateInfo		= str_replace('/strong>',"/strong><br/>",$currentUserProfile['cv']['html']['private-info']);
