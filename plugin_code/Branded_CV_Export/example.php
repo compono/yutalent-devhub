50292198288b0d132291cc615fdf0983b68d1831
Saurav <saurav@anylinuxwork.com>
2013-07-30 16:16:41 +0530

    Add test file to download pdf

--- plugin_code/Branded_CV_Export/example.php
+++ plugin_code/Branded_CV_Export/example.php
@@ -1,157 +1,25 @@
 <?php
-//============================================================+
-// File name   : example_009.php
-// Begin       : 2008-03-04
-// Last Update : 2013-05-14
-//
-// Description : Example 009 for TCPDF class
-//               Test Image
-//
-// Author: Nicola Asuni
-//
-// (c) Copyright:
-//               Nicola Asuni
-//               Tecnick.com LTD
-//               www.tecnick.com
-//               info@tecnick.com
-//============================================================+
 
-/**
- * Creates an example PDF TEST document using TCPDF
- * @package com.tecnick.tcpdf
- * @abstract TCPDF - Example: Test Image
- * @author Nicola Asuni
- * @since 2008-03-04
- */
+require_once('../tcpdf.php');
+$imagepath = $_REQUEST['img'];
+$filename_from_url = parse_url($_REQUEST['img']);
+$ext = pathinfo($filename_from_url['path'], PATHINFO_EXTENSION);
+$imageName = time().$ext;
+$uploadImgPath = __DIR__ . '/upload_image/';
+$file = tempnam( $uploadImgPath, 'tcpdf').'.'.$ext;
+file_put_contents($file,file_get_contents($imagepath));
 
-// Include the main TCPDF library (search for installation path).
-require_once('tcpdf_include.php');
+//$image = '/home/vitaly/user_icon.png';
 
-// create new PDF document
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-
-// set document information
-$pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Nicola Asuni');
-$pdf->SetTitle('TCPDF Example 009');
-$pdf->SetSubject('TCPDF Tutorial');
-$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
-
-// set default header data
-$pdf->SetHeaderData('', PDF_HEADER_LOGO_WIDTH, '', '');
-
-// set header and footer fonts
-$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-
-// set default monospaced font
-$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-
-// set margins
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-
-// set auto page breaks
-$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+$pdf = new TCPDF();
 
 // set image scale factor
 $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
-// set some language-dependent strings (optional)
-if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
-	require_once(dirname(__FILE__).'/lang/eng.php');
-	$pdf->setLanguageArray($l);
-}
-
-// -------------------------------------------------------------------
-
-// add a page
 $pdf->AddPage();
 
-// set JPEG quality
-$pdf->setJPEGQuality(75);
+$pdf->Image($file, 0, 0, 20, 20, '', 'http://www.tcpdf.org', '', true, 150, '', false, false, 1, false, false, false);
 
-// Image method signature:
-// Image($file, $x='', $y='', $w=0, $h=0, $type='', $link='', $align='', $resize=false, $dpi=300, $palign='', $ismask=false, $imgmask=false, $border=0, $fitbox=false, $hidden=false, $fitonpage=false)
+$pdf->Output('doc.pdf', 'I');
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-// Example of Image from data stream ('PHP rules')
-
-//$filename = file_get_contents("http://anylinuxwork.in/demonew/saurav/tcpdf/examples/images/image_with_alpha.png");
-//$imgdata = base64_decode($filename);//'iVBORw0KGgoAAAANSUhEUgAAABwAAAASCAMAAAB/2U7WAAAABlBMVEUAAAD///+l2Z/dAAAASUlEQVR4XqWQUQoAIAxC2/0vXZDrEX4IJTRkb7lobNUStXsB0jIXIAMSsQnWlsV+wULF4Avk9fLq2r8a5HSE35Q3eO2XP1A1wQkZSgETvDtKdQAAAABJRU5ErkJggg==');
-
-// The '@' character is used to indicate that follows an image data stream and not an image file name
-//$pdf->Image('@'.$imgdata);
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-// Image example with resizing
-
-try{
-$image = $_REQUEST['img'] ? $_REQUEST['img'] : 'http://anylinuxwork.in/demonew/saurav/tcpdf/examples/images/logo_example.jpg';
-$pdf->Image($image, 0, 0, 20, 20, '', 'http://www.tcpdf.org', '', true, 150, '', false, false, 1, false, false, false);
-}
-catch(Exception $e)
-{echo $e;die;}
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-// test fitbox with all alignment combinations
-
-$horizontal_alignments = array('L', 'C', 'R');
-$vertical_alignments = array('T', 'M', 'B');
-
-/*$x = 15;
-$y = 35;
-$w = 30;
-$h = 30;
-$i =0;
-$j =0;
-// test all combinations of alignments
-//for ($i = 0; $i < 3; ++$i) {
-	$fitbox = $horizontal_alignments[$i].' ';
-	$x = 15;
-	//for ($j = 0; $j < 3; ++$j) {
-		$fitbox{1} = $vertical_alignments[$j];
-		$pdf->Rect($x, $y, $w, $h, 'F', array(), array(128,255,128));
-		$pdf->Image('images/image_demo.jpg', $x, $y, $w, $h, 'JPG', '', '', false, 300, '', false, false, 0, $fitbox, false, false);
-		$x += 32; // new column
-	//}
-	$y += 32; // new row
-//}
-
-$x = 115;
-$y = 35;
-$w = 25;
-$h = 50;
-$i =0;
-$j =0;
-//for ($i = 0; $i < 3; ++$i) {
-	$fitbox = $horizontal_alignments[$i].' ';
-	$x = 115;
-	//for ($j = 0; $j < 3; ++$j) {
-		$fitbox{1} = $vertical_alignments[$j];
-		$pdf->Rect($x, $y, $w, $h, 'F', array(), array(128,255,255));
-		$pdf->Image('images/image_demo.jpg', $x, $y, $w, $h, 'JPG', '', '', false, 300, '', false, false, 0, $fitbox, false, false);
-		$x += 27; // new column
-	//}
-	$y += 52; // new row
-//}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-// Stretching, position and alignment example
-
-$pdf->SetXY(110, 200);
-$pdf->Image('images/image_demo.jpg', '', '', 40, 40, '', '', 'T', false, 300, '', false, false, 1, false, false, false);
-$pdf->Image('images/image_demo.jpg', '', '', 40, 40, '', '', '', false, 300, '', false, false, 1, false, false, false);
-*/
-// -------------------------------------------------------------------
-
-//Close and output PDF document
-$pdf->Output('example_009.pdf', 'I');
-
-//============================================================+
-// END OF FILE
-//============================================================+
+ exit;
