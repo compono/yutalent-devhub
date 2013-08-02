02d24510e0cbaaf88820946df2f106281b627a58
Saurav <saurav@anylinuxwork.com>
2013-08-02 16:42:38 +0530

    Add subject for reject auto respond

--- plugin_code/Reject_Auto_Responder/config.inc.php
+++ plugin_code/Reject_Auto_Responder/config.inc.php
@@ -3,5 +3,5 @@ define("wuDomain","www.wutalent.co.uk");
 define('HTTP_SSL','https');
 define('TEST_SMTP_MAIL_BODY',"Hi {name}<br/><br/>This is the test email to check smtp setting connection and it is working fine.<br/><br/>Thanks");
 define('TEST_SMTP_CONNCTION',"Test SMTP connection");
-define('DEFAULT_SUBJECT',"Your application to our role '{adverTitle}");
+define('DEFAULT_SUBJECT',"Your application to our role {adverTitle}");
 define('DEFAULT_MAIL_CONTENT',"Dear Candidate<br/><br/>Thankyou for applying for our role. Unfortunately you have not been successful in your application.<br/>We wish you all the best in your search.<br/><br/>Kind regards.");
