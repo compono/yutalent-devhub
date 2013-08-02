e5d7b6883c4801a2e673a7dca7ec2457bc77aeed
Saurav <saurav@anylinuxwork.com>
2013-08-02 16:14:29 +0530

    Change default text of mail content

--- plugin_code/Reject_Auto_Responder/config.inc.php
+++ plugin_code/Reject_Auto_Responder/config.inc.php
@@ -3,4 +3,4 @@ define("wuDomain","www.wutalent.co.uk");
 define('HTTP_SSL','https');
 define('TEST_SMTP_MAIL_BODY',"Hi {name}<br/><br/>This is the test email to check smtp setting connection and it is working fine.<br/><br/>Thanks");
 define('TEST_SMTP_CONNCTION',"Test SMTP connection");
-define('DEFAULT_MAIL_CONTENT',"Dear {name}<br/><br/>Thankyou for applying our role {jobTitle}. Unfortunately you have not been successful in your application.<br/>We wish you all the best in your search.<br/><br/>Kind Regards");
+define('DEFAULT_MAIL_CONTENT',"Dear Candidate<br/><br/>Thankyou for applying for our role. Unfortunately you have not been successful in your application.<br/>We wish you all the best in your search.<br/><br/>Kind regards.");
