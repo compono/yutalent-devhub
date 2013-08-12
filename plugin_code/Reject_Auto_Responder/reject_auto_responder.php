92982ed2286da818574ae806ab2b8d5d854b9c51
Saurav Jain <saurav@anylinuxwork.com>
2013-08-12 15:25:54 +0530

    Removing unused files

--- plugin_code/Reject_Auto_Responder/reject_auto_responder.php
+++
@@ -1,7 +0,0 @@
-<?php require_once('config.inc.php');
-?><script>var wuDomain = '<?php echo wuDomain; ?>';
-var HTTP_SSL = '<?php echo HTTP_SSL; ?>';
-var DEFAULT_MAIL_CONTENT = '<?php echo DEFAULT_MAIL_CONTENT; ?>';
-var DEFAULT_SUBJECT = '<?php echo DEFAULT_SUBJECT; ?>';
-</script>
-<?php require_once('views/smtpSettingForm.php');
