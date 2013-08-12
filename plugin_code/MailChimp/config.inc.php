e85aa172bfba0fa47e6662ded705665ef80d27ab
Saurav Jain <saurav@anylinuxwork.com>
2013-08-12 15:24:56 +0530

    Removing unused files

--- plugin_code/MailChimp/config.inc.php
+++
@@ -1,5 +0,0 @@
-<?php 
-define("DEV", "d/plugin_dev/plugin_code/MailChimp/");
-define("PROD", "d/plugin_prod/plugin_code/MailChimp/");
-define("wuDomain","www.wutalent.co.uk");
-define('HTTP_SSL','https');
