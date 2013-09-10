373e9718870bbae91f707b574692ea7fa353ed60
Andre <a.zerstoren@gmail.com>
2013-09-10 20:50:37 +0300

    changed path

--- plugin_code/SMS/config.inc.php
+++ plugin_code/SMS/config.inc.php
@@ -1,6 +1,6 @@
 <?php 
 //defining file path for both devloper and product version.
-define("SITE_URL",     "/home/devhub.wutalent.co.uk/");
+define("SITE_URL",     "/home/devhub.vutalent.com/");
 define("DEV",     "public_html/plugin_code/SMS/");
 define("PROD",     "plugin_code/SMS_2Way/");
 ?>
