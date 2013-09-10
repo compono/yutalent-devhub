be8a0169a608bbd45d1f70408afac1079d6cab8c
Andre <a.zerstoren@gmail.com>
2013-09-10 21:34:48 +0300

    path

--- plugin_code/SMS/config.inc.php
+++ plugin_code/SMS/config.inc.php
@@ -1,6 +1,6 @@
 <?php 
 //defining file path for both devloper and product version.
-define("SITE_URL",     "/home/devhub.vutalent.com/");
+define("SITE_URL",     "/home/devhub.yutalent.com/");
 define("DEV",     "public_html/plugin_code/SMS/");
 define("PROD",     "plugin_code/SMS_2Way/");
 ?>
