de687408071ec4f655e4407628476290ce4c6469
Vitaly Dyatlov <md.xytop@gmail.com>
2013-09-10 19:49:11 +0300

    Update config.inc.php

--- plugin_code/SMS/config.inc.php
+++ plugin_code/SMS/config.inc.php
@@ -1,4 +1,5 @@
 <?php 
+ini_set('display_errors', true);
 //defining file path for both devloper and product version.
 define("SITE_URL",     "/home/devhub.wutalent.co.uk/");
 define("DEV",     "public_html/plugin_code/SMS_2Way/");
