139956ab8bcc052a9b3cdb2901d8c29d882b79a1
Steve Walker <steve@idibu.com>
2013-09-18 17:45:53 +0700

    updating to new beta install

--- plugin_code/Branded_CV_Export/config.inc.php
+++ plugin_code/Branded_CV_Export/config.inc.php
@@ -1,4 +1,4 @@
 <?php
 define('WU_DOMAIN', 'https://www.wutalent.co.uk'); 
-define('WU_ID', '4ce2-ee6463a16-50426db0c-be81040be-ad322');
-define('WU_SECRET', 'xmW5SW84sPLQmP4vsNG9ZDCyHN5tOV1B9N6Rn6NU');
+define('WU_ID', '354D-CB106FD58-C294AA40E-8DABC56C0-F7A39');
+define('WU_SECRET', 'Y8GRUWG15DBHWUIKKUZS9VTADXWP8LRXJXDKLMDG');
