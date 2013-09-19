af4ae7eb40a2ea77b1f5e5a2a9cf43f05d5274a3
MartinTokarski <kontakt@marcint.pl>
2013-09-19 21:43:11 +0200

    Update downloadPdf.php

--- plugin_code/Branded_CV_Export/downloadPdf.php
+++ plugin_code/Branded_CV_Export/downloadPdf.php
@@ -126,7 +126,7 @@ class MYPDF extends TCPDF {
         $this->SetFont('times', '', 14, '', true);
         $this->writeHTMLCell(0, 0, 0, 33, '<table> <tr><th width="100%" align="center" class="candidate-name"> CV: ' . $candidateName . '</th></tr></table>', 0, 0, false, true, '', true);
         $style = array('width' => 0.3, 'phase' => 10, 'color' => array(71, 97, 108));
-        $this->Line(0, 34, 200, 30, $style);
+        $this->Line(0, 40, 30, 40, $style);
 
 
     }
