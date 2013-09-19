6bb05f52e652e4989e0afca7d9d8c9068abf8b97
MartinTokarski <kontakt@marcint.pl>
2013-09-19 21:16:34 +0200

    Update downloadPdf.php

--- plugin_code/Branded_CV_Export/downloadPdf.php
+++ plugin_code/Branded_CV_Export/downloadPdf.php
@@ -116,11 +116,11 @@ class MYPDF extends TCPDF {
     public function Header() {
         global $comProfile, $candidateName, $imagePath, $imageSize;
         $companyName = $comProfile['profile']['company-name'];
-        $this->writeHTMLCell(0, 0, 10, 10, '<table border="0"> <tr><th width="100%" align="center">
+        $writeHTMLCell = $this->writeHTMLCell(0, 0, 10, 10, '<table border="0"> <tr><th width="100%" align="center">
             <img height="' . $imageSize['h'] . 'px" width="' . $imageSize['w'] . 'px" src="' . $imagePath . '" alt="' . $companyName . '" border="0" />
                 </th></tr></table>', 0, 0, false, true, '', true);
-        $this->SetFont('helvetica', '', 8, '', true);
-        $this->writeHTMLCell(0, 0, 48, 10, '<table border="0"> <tr><th width="100%" align="center">' . $companyName . '</th></tr></table>', 0, 0, false, true, '', true);
+        $this->SetFont('helvetica', '',8, '', true);
+        $this->writeHTMLCell(0, 0, 200, 10, '<table border="0"> <tr><th width="100%" align="center">' . $companyName . '</th></tr></table>', 0, 0, false, true, '', true);
         $this->SetFont('helvetica', '', 14, '', true);
         $this->writeHTMLCell(0, 0, 48, 18, '<table border="0"> <tr><th width="100%" align="center" class="candidate-name"> CV: ' . $candidateName . '</th></tr></table>', 0, 0, false, true, '', true);
         $style = array('width' => 0.3, 'phase' => 10, 'color' => array(222, 222, 222));
