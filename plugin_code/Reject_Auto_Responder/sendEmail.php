90ffa9b863625a6abaec7127ca4666a0c2040da4
Saurav Jain <saurav@anylinuxwork.com>
2013-09-19 17:03:26 +0530

    Add remote address condition and get dynamic domain name

--- plugin_code/Reject_Auto_Responder/sendEmail.php
+++ plugin_code/Reject_Auto_Responder/sendEmail.php
@@ -1,15 +1,19 @@
 <?php 
-extract($_REQUEST);
-require_once('config.inc.php');
-if(!empty($fromEmail) && !empty($fromName) && !empty($hostServer) && !empty($userName) && !empty($password) && !empty($port))
-{	// to required value exist or not
-	require_once('libraries/email.php');	
-	if(isset($testConnection))
-	{
-		$toEmail = $fromEmail;
-		$body = str_replace('{name}',ucwords($name),TEST_SMTP_MAIL_BODY);
-		$subject = TEST_SMTP_CONNCTION;
-	}	
-	$sendEmail = new SmtpMail($userName,$password,$toEmail,$fromEmail,$fromName,$subject,$body,$hostServer,$port,$smtpNumber);
-	$sendEmail->sendEmail();
+$whiteListIp = array('95.138.185.73','54.251.162.52');
+if(in_array($_SERVER['REMOTE_ADDR'],$whiteListIp))	// check script is calling locally or not
+{
+	extract($_REQUEST);
+	require_once('config.inc.php');
+	if(!empty($fromEmail) && !empty($fromName) && !empty($hostServer) && !empty($userName) && !empty($password) && !empty($port))
+	{	// to required value exist or not
+		require_once('libraries/email.php');	
+		if(isset($testConnection))
+		{
+			$toEmail = $fromEmail;
+			$body = str_replace('{name}',ucwords($name),TEST_SMTP_MAIL_BODY);
+			$subject = TEST_SMTP_CONNCTION;
+		}	
+		$sendEmail = new SmtpMail($userName,$password,$toEmail,$fromEmail,$fromName,$subject,$body,$hostServer,$port,$smtpNumber);
+		$sendEmail->sendEmail();
+	}
 }
