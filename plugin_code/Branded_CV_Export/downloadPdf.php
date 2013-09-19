72708550ad004be3f6c5fb55446a8fd59d1cdef9
MartinTokarski <kontakt@marcint.pl>
2013-09-19 22:31:31 +0200

    Update downloadPdf.php

--- plugin_code/Branded_CV_Export/downloadPdf.php
+++ plugin_code/Branded_CV_Export/downloadPdf.php
@@ -131,12 +131,12 @@ class MYPDF extends TCPDF {
     public function Footer() {
 
         global $comProfile;
-        $this->SetY(-5);
+        $this->SetY(-15);
         $comProfile['profile']['www']['address'];
         $this->SetFont('helvetica', '', 8, '', true);
         $this->SetTextColorArray(array(120, 129, 132));
-        $this->Cell(0, 0, $comProfile['profile']['address'] , 0, 0, 'C', 0, '', 0);
-        $this->Cell(0, 0, $comProfile['profile']['www'], 0, 0, 'C', 0, '', 1);
+        $this->Cell(0, 0, $comProfile['profile']['address'] , 1, 1, 'C', 0, '', 0);
+        $this->Cell(0, 10, $comProfile['profile']['www'], 1, 1, 'C', 0, '', 1);
     }
 
 }
