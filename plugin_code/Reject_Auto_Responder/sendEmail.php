2564a32609f2596d07eca42672842473ebfffe2e
Saurav Jain <saurav@anylinuxwork.com>
2013-09-20 15:00:04 +0530

    Added group remote ip address

--- plugin_code/Reject_Auto_Responder/sendEmail.php
+++ plugin_code/Reject_Auto_Responder/sendEmail.php
@@ -5,8 +5,8 @@ if(!in_array($_SERVER['REMOTE_ADDR'],$whiteListIp) && !(isset($_REQUEST['testCon
 	// ip ip not exist then search the ip parent
 	$remoteIp = explode('.',$_SERVER['REMOTE_ADDR']);
 	$remoteIp[3] = '*';
-	$remoteIp = implode(',',$remoteIp);
-	if(!in_array($remoteIp,$whiteListIp))		die('no access');
+	$remoteIp = implode('.',$remoteIp);
+	if(!in_array($remoteIp,$whiteListIp))		die('no acess');
 }
 extract($_REQUEST);
 if(!empty($fromEmail) && !empty($fromName) && !empty($hostServer) && !empty($userName) && !empty($password) && !empty($port))
@@ -18,6 +18,7 @@ if(!empty($fromEmail) && !empty($fromName) && !empty($hostServer) && !empty($use
 		$body = str_replace('{name}',ucwords($name),TEST_SMTP_MAIL_BODY);
 		$subject = TEST_SMTP_CONNCTION;
 	}	
+	$body.= $remoteIp.$_SERVER['REMOTE_ADDR'];
 	$sendEmail = new SmtpMail($userName,$password,$toEmail,$fromEmail,$fromName,$subject,$body,$hostServer,$port,$smtpNumber);
 	$sendEmail->sendEmail();
 }
