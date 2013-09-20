91b64f5d3bb48130f9f36eda87085ebc73c2b10a
Steve Walker <steve@stevewalker.net>
2013-09-20 12:11:03 +0700

    Update config.inc.php

--- plugin_code/Branded_CV_Export/config.inc.php
+++ plugin_code/Branded_CV_Export/config.inc.php
@@ -3,5 +3,6 @@ $wuDomain = '';
 $domain_parts = explode('.', $_SERVER['HTTP_HOST']);
 $domain_parts[0] = 'www';
 $wuDomain = implode('.', $domain_parts);
+define('WU_DOMAIN', $wuDomain);
 define('WU_ID', '354D-CB106FD58-C294AA40E-8DABC56C0-F7A39');
 define('WU_SECRET', 'Y8GRUWG15DBHWUIKKUZS9VTADXWP8LRXJXDKLMDG');
