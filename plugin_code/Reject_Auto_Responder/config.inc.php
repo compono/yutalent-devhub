9e558dcb996c14530c30832ec9f70f4d3a4818cd
Saurav Jain <saurav@anylinuxwork.com>
2013-09-20 12:24:01 +0530

    working for add database var

--- plugin_code/Reject_Auto_Responder/config.inc.php
+++ plugin_code/Reject_Auto_Responder/config.inc.php
@@ -5,11 +5,12 @@ $domainName = $_SERVER['SERVER_NAME'];
 if (preg_match('/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $domainName, $resultDomainName)) {
 	$wuDomain = 'www.'.$resultDomainName['domain'];
 }
-define("wuDomain",'www.wutalent.co.uk');
-define('HTTP_SSL','https');
+define("wuDomain",$wuDomain);
+define('HTTP_SSL',((isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on")?'https':'http'));
+define('WU_DOMAIN', HTTP_SSL.'://'.wuDomain);
 define('TEST_SMTP_MAIL_BODY',"Hi {name}<br/><br/>This is the test email to check smtp setting connection and it is working fine.<br/><br/>Thanks");
 define('TEST_SMTP_CONNCTION',"Test SMTP connection");
 define('DEFAULT_SUBJECT',"Your application to our role {adverTitle}");
 define('DEFAULT_MAIL_CONTENT',"Dear {firstname|ifempty|Candidate}<br/><br/>Thankyou for applying for our role. Unfortunately you have not been successful in your application.<br/>We wish you all the best in your search.<br/><br/>Kind regards.");
-define('WU_ID', 'af52-90a93042b-7a6f1b89c-c6d202471-4dccc');
-define('WU_SECRET', 'PUejZK1MaHbOJA0RsHGkNcsS5WLvZJXpd4O5EIHg');
+define('WU_ID', 'AF52-90A93042B-7A6F1B89C-C6D202471-4DCCC');
+define('WU_SECRET', 'PUEJZK1MAHBOJA0RSHGKNCSS5WLVZJXPD4O5EIHG');
