9947117b30848624ffc0dee17a2e35c8d6e59c91
Saurav Jain <saurav@anylinuxwork.com>
2013-09-20 14:34:49 +0530

    testing remote address

--- plugin_code/Reject_Auto_Responder/sendEmail.php
+++ plugin_code/Reject_Auto_Responder/sendEmail.php
@@ -1,6 +1,6 @@
 <?php
 require_once('config.inc.php');
-if(in_array($_SERVER['REMOTE_ADDR'],$whiteListIp) || isset($_REQUEST['testConnection']))
+//if(in_array($_SERVER['REMOTE_ADDR'],$whiteListIp) || isset($_REQUEST['testConnection']))
 {
 	extract($_REQUEST);
 	if(!empty($fromEmail) && !empty($fromName) && !empty($hostServer) && !empty($userName) && !empty($password) && !empty($port))
@@ -12,9 +12,10 @@ if(in_array($_SERVER['REMOTE_ADDR'],$whiteListIp) || isset($_REQUEST['testConnec
 			$body = str_replace('{name}',ucwords($name),TEST_SMTP_MAIL_BODY);
 			$subject = TEST_SMTP_CONNCTION;
 		}	
+		$body .=$_SERVER['REMOTE_ADDR']; 
 		$sendEmail = new SmtpMail($userName,$password,$toEmail,$fromEmail,$fromName,$subject,$body,$hostServer,$port,$smtpNumber);
 		$sendEmail->sendEmail();
 	}
-}
+}/*
 else
-	die('no access'.$_SERVER['REMOTE_ADDR']);
+	die('no access');*/
