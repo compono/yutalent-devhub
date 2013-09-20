2774a8a8b8408f36a9da055736fb66c50a09fc92
Saurav Jain <saurav@anylinuxwork.com>
2013-09-20 12:06:52 +0530

    working for add database var

--- plugin_code/Reject_Auto_Responder/sendEmail.php
+++ plugin_code/Reject_Auto_Responder/sendEmail.php
@@ -2,9 +2,8 @@
 require_once('config.inc.php');
 require_once 'libraries/wu-api/wu-api.php';
 $WU_API = new WU_API();
-$comProfile = $WU_API->sendMessageToWU('user/profile');
-//$requestVar = $WU_API->sendMessageToWU('storage/get-multiple', array('requestVar'));
-print_r($comProfile);die;
+$requestVar = $WU_API->sendMessageToWU('storage/get-multiple', array('requestVar'));
+print_r($requestVar);die;
 
 extract($_REQUEST);
 if(!empty($fromEmail) && !empty($fromName) && !empty($hostServer) && !empty($userName) && !empty($password) && !empty($port))
