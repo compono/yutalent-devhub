ffa0404157b973896cba48c0463e3ed488e4b8f9
Steve Walker <steve@idibu.com>
2013-10-20 11:15:31 +0700

    domain update

--- plugin_code/Branded_CV_Export/libraries/tcpdf/core/images/testPDF.php
+++ plugin_code/Branded_CV_Export/libraries/tcpdf/core/images/testPDF.php
@@ -2,7 +2,7 @@
 $scriptUrl = ((isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on")?'https':'http') . '://' . $_SERVER['HTTP_HOST'].'/'.$_SERVER['PHP_SELF'];
 require 'wu-api/wu-api.php';
 
-define('WU_DOMAIN', 'https://www.wutalent.co.uk');
+define('WU_DOMAIN', 'https://www.yutalent.co.uk');
 
 define('WU_ID', '4ce2-ee6463a16-50426db0c-be81040be-ad322');
 define('WU_SECRET', 'xmW5SW84sPLQmP4vsNG9ZDCyHN5tOV1B9N6Rn6NU');
--- plugin_code/MailChimp/config.inc.php
+++ plugin_code/MailChimp/config.inc.php
@@ -1,5 +1,5 @@
 <?php 
 define("DEV", "d/plugin_dev/plugin_code/MailChimp/");
 define("PROD", "d/plugin_prod/plugin_code/MailChimp/");
-define("wuDomain","www.wutalent.co.uk");
+define("wuDomain","www.yutalent.co.uk");
 define('HTTP_SSL','https');
