1c253cc75d2dfa23a361b89565351f7dc196d7a6
Michał Jasikowski <michal@jasikowski.pl>
2013-12-18 19:15:58 +0100

    Updated SMS_2Way

--- plugin_code/SMS_2Way/inbound.php
+++ plugin_code/SMS_2Way/inbound.php
@@ -27,6 +27,10 @@ $WU_API->setAccountId($accountId);
 
 $contactId = $WU_API->sendMessageToWU('storage/get', array('key' => 'mobile_'.$msg->from));
 
+if (isset($contactId))
+    $contactId = $contact[0];
+else die('Contact ID not found');
+
 $note = $WU_API->sendMessageToWU('notes/add', array(
     'contact_id' => $contactId,
     'received' => 'true',
