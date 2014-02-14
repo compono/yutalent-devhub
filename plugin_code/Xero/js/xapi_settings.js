25a4bda75bbb61637bc70724f310cbc5996a0d2f
Dmitriy Dzyuba <dmitriy.dzyuba@smoozeedev.com>
2014-02-14 15:28:19 +0200

    fix js error

--- plugin_code/Xero/js/xapi_settings.js
+++ plugin_code/Xero/js/xapi_settings.js
@@ -4,13 +4,14 @@ window.wuAfterInit = function(wu) {
 	//saving the keys to the storage
 	$('#submit-api').click(function(){
 		wu.Messenger.sendMessageToWU('storage/add-multiple',{
-	    append: false,
-	    pairs: {
-	        xero_consumer_key: $('#xero_consumer_key').val(),
-	        xero_consumer_secret: $('#xero_consumer_secret').val()
-	    }}, function(response) {
-	            $('#xero_api_settings').submit();
-	        });
-	    });
+			append: false,
+			pairs: {
+				xero_consumer_key: $('#xero_consumer_key').val(),
+				xero_consumer_secret: $('#xero_consumer_secret').val()
+			}
+		}, 
+		function(response) {
+			$('#xero_api_settings').submit();
+		});
 	});
 }
