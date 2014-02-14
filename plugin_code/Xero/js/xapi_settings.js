f1b63c61bb4e5e6067397b2f0474a51272ca68cd
Dmitriy Dzyuba <dmitriy.dzyuba@smoozeedev.com>
2014-02-14 12:32:04 +0200

    save settings

--- plugin_code/Xero/js/xapi_settings.js
+++ plugin_code/Xero/js/xapi_settings.js
@@ -1,5 +1,22 @@
-window.wuAfterInit = function(wu) {        
-    wu.Messenger.sendMessageToWU('user/profile', {}, function(response){
-        alert( response.user );
-    });
+window.wuAfterInit = function(wu) {
+	var form_keys = ['xero_consumer_key', 'xero_consumer_secret'];
+
+	wu.Messenger.sendMessageToWU('storage/get-multiple', { keys: form_keys }, function(response) {
+		console.log('multiple', response);
+	});
+
+	$('#submit-api').click(function(){
+		wu.Messenger.sendMessageToWU('storage/add-multiple',{
+	    append: false,
+	    pairs: {
+	        xero_consumer_key: $('#xero_consumer_key'),
+	        xero_consumer_secret: $('#xero_consumer_secret')
+	    }}, function(response) {
+	        console.log( response);
+
+	        wu.Messenger.sendMessageToWU('storage/get-multiple',{ keys: form_keys }, function(response) {
+	            console.log('multiple', response);
+	        });
+	    });
+	});
 }
